e0926b5c17962a049c1fdfeff19ff589b8b0da3f
sverkvl <itachikoken@gmail.com>
2023-02-15 14:58:16 +0600

    fourth commit

--- Java.java
+++ Java.java
@@ -1,2 +1,11 @@
-public class Java {
+import java.util.*;
+class UserInputDemo1
+{
+    public static void main(String[] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter your Name, Surname, Date of birth: ");
+        String str= sc.nextLine();              //reads string
+        System.out.print("Data is censored");
+    }
 }
